84f1c65b5f66979e6fc513faa4e1bd32fca3f9cd
Jasery <jaserydev@163.com>
2023-12-01 14:43:34 +0800

    update m-ui

--- snow-ball/config/commonConfig.js
+++ snow-ball/config/commonConfig.js
@@ -1,19 +1,19 @@
-window._commonConfig  ={
-  "replaceByRemote": false,
-  "runEnv": "local",
-  "envs":{
-    "local": {
-      "serverHost": "http://router.d24.mingchao.com",
-      "metlHost": "http://cdh-log.mingchao.com/metl/v1",
-      "cdnHost": "/GameAssets/"
+window._commonConfig = {
+  replaceByRemote: false,
+  runEnv: "local",
+  envs: {
+    local: {
+      serverHost: "http://router.d24.mingchao.com",
+      metlHost: "http://cdh-log.mingchao.com/metl/v1",
+      cdnHost: "GameAssets/",
     },
-    "debug": {
-      "serverHost": "http://router.m24.mingchao.com",
-      "metlHost": "http://cdh-log.mingchao.com/metl/v1"
+    debug: {
+      serverHost: "http://router.m24.mingchao.com",
+      metlHost: "http://cdh-log.mingchao.com/metl/v1",
     },
-    "release": {
-      "serverHost": "https://xcx-router.mingchaoyouxi.com",
-      "metlHost": "https://cdh-log.mingchaoyouxi.com/metl/v1"
-    }
-  }
-}
+    release: {
+      serverHost: "https://xcx-router.mingchaoyouxi.com",
+      metlHost: "https://cdh-log.mingchaoyouxi.com/metl/v1",
+    },
+  },
+};
